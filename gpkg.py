e7b78925fa225f022cbb5c1a70093579e7b6d374
mdermentzi <mdermentzi@users.noreply.github.com>
2022-06-27 13:27:58 +0100

    Removed unecessary code

--- gpkg.py
+++ gpkg.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
-from streamlit_folium import st_folium
-import folium
 
 st.title('Geopackage Viewer')
 
@@ -11,7 +9,6 @@ uploaded_gpkg = st.sidebar.file_uploader("Upload a .gpkg file to inspect its con
 
 if uploaded_gpkg is not None:
     gdf = gpd.read_file(uploaded_gpkg)
-    gdf_list = [[point.xy[1][0], point.xy[0][0]] for point in gdf.geometry]
 
     gdf['lon'] = gdf.geometry.apply(lambda p: p.x)
     gdf['lat'] = gdf.geometry.apply(lambda p: p.y)
